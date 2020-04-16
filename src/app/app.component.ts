312b53aac6bfcf316ee9e25f243a9cd0446122cf
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-04-16 19:00:32 +0200

    feat: load text document content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ictusnet';
+  document: string;
+  hola = 'hola';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('assets/document.txt', { responseType: 'text' }).subscribe(
+      data => this.document = data,
+      err => console.error(err),
+      () => console.log(typeof(this.document))
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
