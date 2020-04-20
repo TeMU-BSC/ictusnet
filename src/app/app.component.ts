e076b9179c2116a602222eea28ba196630d5ca61
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-04-20 14:33:05 +0200

    feat: get selected text and offsets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,29 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
   title = 'ictusnet';
   document: string;
-  hola = 'hola';
+  selection: Selection;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get('assets/document.txt', { responseType: 'text' }).subscribe(
-      data => this.document = data,
-      err => console.error(err),
-      () => console.log(typeof(this.document))
-    );
+    this.http.get('assets/document.txt', { responseType: 'text' }).subscribe(data => this.document = data);
   }
+
+  saveSpan(event) {
+    if (window.getSelection().toString()) {
+      this.selection = window.getSelection();
+    }
+
+    // this.selectedText = this.selection.toString();
+    // this.start = this.selection.getRangeAt(0).startOffset;
+    // this.end = this.selection.getRangeAt(0).endOffset;
+    // if (this.selectedText.length) {
+    //   console.log(this.text, this.start, this.end);
+    //   alert(`Selected text: ${this.text}\nStart: ${this.start}\nEnd: ${this.end}`);
+    // }
+  }
+
 }
