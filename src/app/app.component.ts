d4274143c8c66206c56cf614a7626c8baebbc1c2
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-04-26 21:17:04 +0200

    feat: parse variables and admissible values from google drive downloaded doc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Papa } from 'ngx-papaparse';
 
 export interface Annotation {
   id: string;
   category: string;
-  offset: string;
-  start: number;
-  end: number;
+  offset: {
+    start: number;
+    end: number;
+  };
   span: string;
+}
+
+export interface Variable {
+  group: string;
+  variable: string;
+  cardinality: string;
+  getValueFrom: string;
+  admissibleValues: string[];
+  comments: string;
 }
 
 @Component({
@@ -36,11 +47,68 @@ export class AppComponent implements OnInit {
   annPreview: string;
   savedSingleAnnotation: string;
 
-  constructor(private http: HttpClient) { }
+  variables: Variable[] = [];
+  admissibleValues: any;
+
+  constructor(private http: HttpClient, private papa: Papa) { }
 
   ngOnInit() {
     this.http.get('assets/document.txt', { responseType: 'text' }).subscribe(data => this.text = data);
     this.http.get('assets/document.ann', { responseType: 'text' }).subscribe(data => this.annTsv = data);
+
+    // parse the form tsv on google drive (2 sheets):
+
+    let variablesData: any;
+    let admissibleValuesData: any;
+
+    // https://docs.google.com/spreadsheets/d/1BX0m27sVzzd-wtTtDdse__nuTUva6xj_XUW6gK9hsj0/edit#gid=0
+    this.papa.parse('assets/variables.tsv', {
+      download: true,
+      header: true,
+      // delimiter: '\t',
+      skipEmptyLines: true,
+      quoteChar: '',
+      complete: results => variablesData = results.data
+    });
+
+    // https://docs.google.com/spreadsheets/d/1BX0m27sVzzd-wtTtDdse__nuTUva6xj_XUW6gK9hsj0/edit#gid=1512976087
+    this.papa.parse('assets/admissible-values.tsv', {
+      download: true,
+      header: true,
+      // delimiter: '\t',
+      skipEmptyLines: true,
+      quoteChar: '',
+      complete: results => {
+        admissibleValuesData = results.data;
+
+        // build the variables list
+        variablesData.forEach((v: Variable) => {
+          const admissibleValues = [];
+          admissibleValuesData.forEach(a => {
+            if (v.variable === a.variable) {
+
+              console.log(v.variable, a.variable);
+
+              admissibleValues.push(a.admissibleValue);
+            }
+          });
+          this.variables.push({
+            group: v.group,
+            variable: v.variable,
+            cardinality: v.cardinality,
+            getValueFrom: v.getValueFrom,
+            admissibleValues,
+            comments: v.comments,
+          });
+        });
+
+        console.log(this.variables);
+      }
+    });
+
+
+
+
   }
 
   showSelection() {
@@ -52,6 +120,9 @@ export class AppComponent implements OnInit {
       this.annPreview = `T${this.id}\t${this.category}\t${this.start} ${this.end}\t${this.selectedText}\n` +
         `#${this.id}\t${this.notes} T${this.id}\t${this.note}`;
     }
+
+    console.log(this.variables);
+    console.log(this.admissibleValues);
   }
 
   saveAnn() {
@@ -68,9 +139,10 @@ export class AppComponent implements OnInit {
       this.annotations.push({
         id: match[1],
         category: match[2],
-        offset: match[3],
-        start: Number(match[4]),
-        end: Number(match[5]),
+        offset: {
+          start: Number(match[4]),
+          end: Number(match[5]),
+        },
         span: match[6]
       });
     }
