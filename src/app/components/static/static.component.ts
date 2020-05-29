4dc61f904bfdaca2119327c9ba514fa629fcba85
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-05-29 17:13:21 +0200

    style: make accordion component and its form fields responsive

--- src/app/components/static/static.component.ts
+++ src/app/components/static/static.component.ts
@@ -1,77 +1,17 @@
-import { Component } from '@angular/core';
-
-// chips autocomplete
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { ElementRef, ViewChild } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
-import { MatChipInputEvent } from '@angular/material/chips';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
-
-// expansion panel
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { MatAccordion } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-static',
   templateUrl: './static.component.html',
-  // templateUrl: './static.component-plain.html',
+  // templateUrl: './simple.component.html',
   styleUrls: ['./static.component.scss']
 })
 export class StaticComponent {
 
-  // chips autocomplete
-  visible = true;
-  selectable = true;
-  removable = true;
-  separatorKeysCodes: number[] = [ENTER, COMMA];
-  fruitCtrl = new FormControl();
-  filteredFruits: Observable<string[]>;
-  fruits: string[] = [];
-  allFruits: string[] = [
-    'arteria carótida común (ACC) o primitiva',
-    'arteria cerebral media (ACM)',
-    'arteria cerebelosa posteroinferior (PICA)',
-    'indeterminada'
-  ];
-  @ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement>;
-  @ViewChild('auto') matAutocomplete: MatAutocomplete;
-  constructor() {
-    this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
-      startWith(null),
-      map((fruit: string | null) => fruit ? this._filter(fruit) : this.allFruits.slice()));
-  }
-  add(event: MatChipInputEvent): void {
-    const input = event.input;
-    const value = event.value;
-    // Add our fruit
-    if ((value || '').trim()) {
-      this.fruits.push(value.trim());
-    }
-    // Reset the input value
-    if (input) {
-      input.value = '';
-    }
-    this.fruitCtrl.setValue(null);
-  }
-  remove(fruit: string): void {
-    const index = this.fruits.indexOf(fruit);
-    if (index >= 0) {
-      this.fruits.splice(index, 1);
-    }
-  }
-  selected(event: MatAutocompleteSelectedEvent): void {
-    this.fruits.push(event.option.viewValue);
-    this.fruitInput.nativeElement.value = '';
-    this.fruitCtrl.setValue(null);
-  }
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    return this.allFruits.filter(fruit => fruit.toLowerCase().indexOf(filterValue) === 0);
-  }
-
-  //expansion panel
   @ViewChild(MatAccordion) accordion: MatAccordion;
+  @ViewChild('downloadButton') downlaodButton: HTMLElement;
+
   step: number = 0;
   setStep(index: number) {
     this.step = index;
@@ -81,6 +21,19 @@ export class StaticComponent {
   }
   prevStep() {
     this.step--;
+  }
+
+  confirm() {
+    confirm('¿Estas seguro?');
+  }
+
+  finish() {
+    this.accordion.closeAll();
+    this.downlaodButton.focus();
+  }
+
+  download() {
+    // TODO (copy from what I've already done in the other component)
   }
 
 }
