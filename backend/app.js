57d147e7d71b58e399d7f345e0a927659b73bbb7
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-11-16 23:06:07 +0100

    feat: upload directory check to store uploaded user .txt ictus report files

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,29 @@
+const fs = require('fs')
 const express = require('express')
 const multer = require('multer')
+const cors = require('cors')
 
 const app = express()
 const port = 3000
-
 const dir = 'uploads'
+
+fs.mkdir(dir, { recursive: true }, (err) => {
+  if (err) throw err;
+})
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, dir),
   filename: (req, file, cb) => cb(null, file.originalname)
 })
 const upload = multer({ storage: storage })
 
+app.use(cors())
+
 app.get('/', (req, res) => {
   res.json({ 'greeting': 'hello from express.' })
-});
+})
 
 app.post('/upload', upload.array('uploads[]'), (req, res) => {
   res.json({ 'message': `Files uploaded successfully to "./backend/${dir}/".` })
-});
+})
 
 app.listen(port, () => console.log(`ICTUSnet express listening on port ${port}!`))
