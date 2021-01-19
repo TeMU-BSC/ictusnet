9a0d5c659f709f7c93e7d45ac43a624ccb49dcea
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2021-01-20 00:45:10 +0100

    fix: uncomment apiUrl environment variable

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://localhost:3000'  // testing production environment in local
-  // apiUrl: 'http://temu.bsc.es:3000'  // deployment in real server
+  // apiUrl: 'http://localhost:3000',  // testing production environment in local
+  apiUrl: 'http://temu.bsc.es:3000',  // deployment in real server
 }
